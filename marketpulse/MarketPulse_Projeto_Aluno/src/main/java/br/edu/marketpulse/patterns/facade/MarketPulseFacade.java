d4b2717c9243be90570ecd6359380672f604022c
MegaXgus <newgustavo098@gmail.com>
2026-09-03 21:37:02 -0300

    Update MarketPulseFacade.java

--- marketpulse/MarketPulse_Projeto_Aluno/src/main/java/br/edu/marketpulse/patterns/facade/MarketPulseFacade.java
+++ marketpulse/MarketPulse_Projeto_Aluno/src/main/java/br/edu/marketpulse/patterns/facade/MarketPulseFacade.java
@@ -1,9 +1,38 @@
 package br.edu.marketpulse.patterns.facade;
-import br.edu.marketpulse.model.*; import br.edu.marketpulse.repository.*; import br.edu.marketpulse.service.*;
+
+import br.edu.marketpulse.model.Investor;
+import br.edu.marketpulse.model.Order;
+import br.edu.marketpulse.model.Quote;
+import br.edu.marketpulse.repository.InMemoryOrderRepository;
+import br.edu.marketpulse.service.TradingApplicationService;
+
 public class MarketPulseFacade {
- private final InMemoryInvestorRepository investors=new InMemoryInvestorRepository();
- private final InMemoryOrderRepository orders=new InMemoryOrderRepository();
- private final TradingApplicationService trading=new TradingApplicationService(investors,orders);
- public void registerInvestor(Investor i){investors.save(i);} public boolean place(Order o){return trading.place(o);} public Quote quote(String ticker){return trading.quote(ticker);}
- public TradingApplicationService getTradingService(){return trading;} public InMemoryOrderRepository getOrderRepository(){return orders;}
+    
+    private final TradingApplicationService trading;
+
+    // Construtor atualizado: recebe o serviço injetado pelo Main
+    public MarketPulseFacade(TradingApplicationService trading) {
+        this.trading = trading;
+    }
+    
+    public void registerInvestor(Investor i) {
+        // Acessa o repositório através da variável 'investors' que é pública no serviço
+        trading.investors.save(i);
+    } 
+    
+    public boolean place(Order o) {
+        return trading.place(o);
+    } 
+    
+    public Quote quote(String ticker) {
+        return trading.quote(ticker);
+    }
+    
+    public TradingApplicationService getTradingService() {
+        return trading;
+    } 
+    
+    public InMemoryOrderRepository getOrderRepository() {
+        return trading.orders;
+    }
 }
