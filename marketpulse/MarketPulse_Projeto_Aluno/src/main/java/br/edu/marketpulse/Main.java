07cf6b59916fe3a0eb8e9c73e57ae525a8148805
MegaXgus <newgustavo098@gmail.com>
2026-09-03 21:36:04 -0300

    Update Main.java

--- marketpulse/MarketPulse_Projeto_Aluno/src/main/java/br/edu/marketpulse/Main.java
+++ marketpulse/MarketPulse_Projeto_Aluno/src/main/java/br/edu/marketpulse/Main.java
@@ -1,23 +1,42 @@
 package br.edu.marketpulse;
 
-import br.edu.marketpulse.model.*;
+import br.edu.marketpulse.model.Investor;
+import br.edu.marketpulse.model.Order;
+import br.edu.marketpulse.model.Quote;
 import br.edu.marketpulse.patterns.facade.MarketPulseFacade;
 import br.edu.marketpulse.patterns.factory.OrderFactory;
-// Importações fictícias das abstrações que justificaremos no ADR
+import br.edu.marketpulse.patterns.observer.MarketPublisher;
 import br.edu.marketpulse.repository.InMemoryInvestorRepository;
 import br.edu.marketpulse.repository.InMemoryOrderRepository;
+import br.edu.marketpulse.service.NotificationService;
+import br.edu.marketpulse.service.QuoteService;
+import br.edu.marketpulse.service.RiskService;
 import br.edu.marketpulse.service.TradingApplicationService;
 
 public class Main {
     public static void main(String[] args) {
         // 1. Configuração de Dependências (Composition Root)
         // Em vez de esconder os "new" dentro do serviço, nós os declaramos aqui e injetamos
-        InMemoryInvestorRepository investorRepo = new InMemoryInvestorRepository();
-        InMemoryOrderRepository orderRepo = new InMemoryOrderRepository();
-        
-        // Injeção de dependência via construtor
-        TradingApplicationService tradingService = new TradingApplicationService(investorRepo, orderRepo);
-        MarketPulseFacade app = new MarketPulseFacade(tradingService); 
+    InMemoryInvestorRepository investorRepo = new InMemoryInvestorRepository();
+    InMemoryOrderRepository orderRepo = new InMemoryOrderRepository();
+    
+    
+    // Instanciando as dependências que estavam faltando
+    QuoteService quoteService = new QuoteService(); 
+    RiskService riskService = new RiskService();
+    NotificationService notificationService = new NotificationService();
+    MarketPublisher marketPublisher = new MarketPublisher();
+
+    // Injeção de dependência via construtor com TODOS os 6 argumentos
+    TradingApplicationService tradingService = new TradingApplicationService(
+        investorRepo, 
+        orderRepo, 
+        quoteService, 
+        riskService, 
+        notificationService, 
+        marketPublisher
+    );
+        MarketPulseFacade app = new MarketPulseFacade(tradingService);
 
         // 2. Execução do fluxo
         app.registerInvestor(new Investor("I1", "aluno@example.com", "CONSERVATIVE", 50000));
